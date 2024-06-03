1bf167b1939070536c7ca435c50676a39d43edb8
unihorn jun <jun@unihorn.com>
2024-06-03 19:44:42 -0400

    add comment for main

--- demo1/src/main/java/com/example/demo/Demo1Application.java
+++ demo1/src/main/java/com/example/demo/Demo1Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Demo1Application {
 
+	//main
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
 	}
-
 }
